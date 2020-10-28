2dbbc7aeffb7c05e409f5994b7be638f91cae9de
A01066270 <a01066270@itesm.mx>
2020-10-28 13:38:21 -0600

    Cambio de input a select

--- _form_zombie.php
+++ _form_zombie.php
@@ -2,14 +2,22 @@
 <section class=" container registro">
     <div class="grid-2">
     <div class="formulario">
-        <form mothod="post">
+        <form action="index.php" mothod="post">
         <div class="card">
             <label class="label">Nombre</label><br>
             <input type="text" class="formulario1" name="nombre" placeholder="Nombre Apellido(s)">
             <br>
             <label class="label">Estado</label><br>
-
-            <input type="text" class="formulario1" name="estado" placeholder="Estado">
+            <div class="custom-select" style="width:200px;">
+  <select>
+    <option value="0">Selecciona un estado:</option>
+    <option value="1">Infeccion</option>
+    <option value="2">Desorientacion</option>
+    <option value="3">Violencia</option>
+    <option value="4">Desmayo</option>
+    <option value="5">Transformacion</option>
+  </select>
+</div>
             <br>
             <br><br>
             <input type="submit" class ="submit" value="Ingresar" >
@@ -66,7 +74,12 @@
     </tr>
     
     <tr>
-        
+        <td></td>
+        <td>Infeccion</td>
+        <td>Desorientacion</td>
+        <td>Violencia</td>
+        <td>Desmayo</td>
+        <td>Transformacion</td>        
     </tr>
     
     </table>
